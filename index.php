f003de6af65479526429c2694b6c1276a79515c7
Jensenk2136 <38008602+Jensenk2136@users.noreply.github.com>
2018-04-16 00:41:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,38 @@
                     <h4 class="modal-title">Rules</h4>
                 </div>
                 <div class="modal-body">
-                    TEST
+                    BETS
+                  <ul>
+                    <li>
+                      Straight
+                      <li>This is a bet that covers only one number. In order to make this bet, place the chip inside the square of the number.</li>
+                    </li>
+                    <li>
+                      Split
+                      <li>A bet on two numbers which are adjacent on the table, made by placing the chip on the shared line of the two numbers’ squares.</li>
+                    </li>
+                    <li>
+                      Street
+                      <li>A bet on three consecutive numbers located on the same line. Make the bet by placing the chip on the outer corner of the row.</li>
+                    </li>
+                    <li>
+                      Six Line
+                      <li>A bet on two adjacent lines. In order to make this bet, you have to place the chip on the common outer corner of the two lines.</li>
+                    </li>
+                    <li>
+                      Corner
+                      <li>This is a four-number bet, placed by putting the chip on the common corner of the four numbers. Also called ‘square’ bet.</li>
+                    </li>
+                    <li>
+                      Trio
+                      <li>A three-number bet that includes the zero or zeros. Place the chip on the line shared by the zero box and the two other numbers.</li>
+                    </li>
+                    <li>
+                      Basket
+                      <li>Bet on 0, 1, 2 and 3 with a chip on the corner shared by the zero box and the first line. In American Roulette, it includes the double zero.</li>
+                    </li>
+                    
+                        
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
